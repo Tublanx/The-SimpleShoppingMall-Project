86df567d4a6803272d2f9fa66a815431543dd23c
Tublanx <rlgus03453@naver.com>
2022-03-23 22:12:57 +0900

    회원 기능 추가(등록, 목록 조회)

--- src/main/java/helloshop/repository/MemberRepository.java
+++ src/main/java/helloshop/repository/MemberRepository.java
@@ -1,2 +1,32 @@
-package helloshop.repository;public class MemberRepository {
+package helloshop.repository;
+
+import helloshop.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // 엔티티 매니저 주입
+    EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); // 회원 엔티티 저장
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // 회원 번호로 회원 엔티티 조회
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from member m where m.name = :name", Member.class).setParameter("name", name).getResultList(); // JPQL을 이용하여 이름으로 회원 엔티티 조회
+    }
+
 }
--- src/test/java/helloshop/service/MemberServiceTest.java
+++ src/test/java/helloshop/service/MemberServiceTest.java
@@ -1,4 +1,46 @@
+package helloshop.service;
+
+import helloshop.entity.Member;
+import helloshop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
-  
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void join() throws Exception {
+        Member member = new Member();
+        member.setName("lgh");
+
+        Long saveId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    void 중복회원예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("lgh");
+
+        Member member2 = new Member();
+        member2.setName("lgh");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+        fail("예외 발생");
+    }
 }
