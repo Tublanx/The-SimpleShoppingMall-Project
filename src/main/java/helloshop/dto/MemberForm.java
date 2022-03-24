f857ea6ed8fd2cd4d04652d2d33eb4820e88a80e
Tublanx <rlgus03453@naver.com>
2022-03-24 15:19:37 +0900

    회원가입 기능 작성

---
+++ src/main/java/helloshop/dto/MemberForm.java
@@ -0,0 +1,42 @@
+package helloshop.dto;
+
+public class MemberForm {
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
