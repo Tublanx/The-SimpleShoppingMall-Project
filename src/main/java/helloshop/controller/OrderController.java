54e237f75f484e022da91fdd705056fbd5293e75
Tublanx <rlgus03453@naver.com>
2022-03-25 20:43:48 +0900

    주문 폼 이동 컨트롤러, 폼 생성 (미완성)

--- src/main/java/helloshop/controller/OrderController.java
+++ src/main/java/helloshop/controller/OrderController.java
@@ -1,14 +1,34 @@
 package helloshop.controller;
 
+import helloshop.entity.Member;
+import helloshop.entity.item.Item;
+import helloshop.service.ItemService;
+import helloshop.service.MemberService;
+import helloshop.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
     @GetMapping("/order")
-    public String order() {
-        return "order";
+    public String order(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "/orders/createOrderForm";
     }
 
 }
